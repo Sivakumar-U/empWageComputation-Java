c49f4a4fd5cc776a4d67c211b78a6aa948ec7730
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-22 18:14:44 +0530

    Added usecase for total wage of different companies in array

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,51 +1,21 @@
 package empwagecomputation;
 
 public class EmpWageBuilder {
-	private static final int FULL_TIME = 1;
-	private static final int PART_TIME = 0;
-
-	private final String companyName;
-	private final int wagePerHour;
-	private final int workingDaysPerMonth;
-	private final int maxHours;
-
-	public EmpWageBuilder(String companyName, int wagePerHour, int workingDaysPerMonth, int maxHours) {
-		this.companyName = companyName;
-		this.wagePerHour = wagePerHour;
-		this.workingDaysPerMonth = workingDaysPerMonth;
-		this.maxHours = maxHours;
-	}
-
-	public void get_EmployeeWageForCompany() {
-		int totalWage = 0;
-		int dailyWage = 0;
-		int totalDays = 0;
-		int totalHours = 0;
-		int workingHours = 0;
-
-		while (totalDays <= workingDaysPerMonth && totalHours < maxHours) {
-			totalDays++;
-			int checkWorking = (int) (Math.random() * 3);
-			switch (checkWorking) {
-			case FULL_TIME:
-				workingHours = 8;
-				break;
-			case PART_TIME:
-				workingHours = 4;
-				break;
-			default:
-				workingHours = 0;
-			}
-			dailyWage = (wagePerHour * workingHours);
-			System.out.println("Day " + totalDays + " Employee Wage for Company " + companyName + " is " + dailyWage);
-			totalWage += dailyWage;
-		}
-		System.out.println("Total Employee Wage for " + companyName + " is " + totalWage);
-	}
-
 	public static void main(String[] args) {
+		Object[] employeeWageComputation = new Object[10];
+		int index = 0;
 		System.out.println("Welcome to Employee Wage Computation");
-		EmpWageBuilder empWageBulider = new EmpWageBuilder("Reliance", 20, 20, 50);
-		empWageBulider.get_EmployeeWageForCompany();
+		CompanyEmployeeWage companyEmployeeWage = new CompanyEmployeeWage("Reliance", 20, 20, 50);
+		employeeWageComputation[index++] = companyEmployeeWage.get_EmployeeWageForCompany();
+
+		companyEmployeeWage = new CompanyEmployeeWage("D-mart", 10, 25, 50);
+		employeeWageComputation[index++] = companyEmployeeWage.get_EmployeeWageForCompany();
+
+		companyEmployeeWage = new CompanyEmployeeWage("metro", 17, 25, 20);
+		employeeWageComputation[index++] = companyEmployeeWage.get_EmployeeWageForCompany();
+
+		for (int index1 = 0; index1 < index; index1++) {
+			System.out.println("Total-wage of " + employeeWageComputation[index1]);
+		}
 	}
 }

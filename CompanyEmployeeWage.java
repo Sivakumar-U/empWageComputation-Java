7c99ed3a92a84f0f52d0882390b4f28ca59ee520
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-22 18:25:27 +0530

    Added usecase for total wage of different companies in Array list

--- CompanyEmployeeWage.java
+++ CompanyEmployeeWage.java
@@ -1,44 +1,44 @@
 package empwagecomputation;
 
-public class CompanyEmployeeWage {
+public class CompanyEmployeeWage<maxHours> {
 	private static final int FULL_TIME = 1;
-    private static final int PART_TIME = 0;
+	private static final int PART_TIME = 0;
 
-    private final String companyName;
-    private final int wagePerHour;
-    private final int workingDaysPerMonth;
-    private final int maxHours;
+	private final String companyName;
+	private final int wagePerHour;
+	private final int workingDaysPerMonth;
+	private final int maxHours;
 
-    public CompanyEmployeeWage(String companyName, int wagePerHour, int workingDaysPerMonth, int maxHours) {
-        this.companyName = companyName;
-        this.wagePerHour = wagePerHour;
-        this.workingDaysPerMonth = workingDaysPerMonth;
-        this.maxHours = maxHours;
-    }
+	public CompanyEmployeeWage(String companyName, int wagePerHour, int workingDaysPerMonth, int maxHours) {
+		this.companyName = companyName;
+		this.wagePerHour = wagePerHour;
+		this.workingDaysPerMonth = workingDaysPerMonth;
+		this.maxHours = maxHours;
+	}
 
-    public String get_EmployeeWageForCompany() {
-        int totalWage = 0;
-        int dailyWage = 0;
-        int totalDays = 0;
-        int totalHours = 0;
-        int workingHours = 0;
+	public String get_EmployeeWageForCompany() {
+		int totalWage = 0;
+		int dailyWage = 0;
+		int totalDays = 0;
+		int totalHours = 0;
+		int workingHours = 0;
 
-        while (totalDays <= workingDaysPerMonth && totalHours < maxHours) {
-            totalDays++;
-            int checkWorking = (int) (Math.random() * 3);
-            switch (checkWorking) {
-                case FULL_TIME:
-                    workingHours = 8;
-                    break;
-                case PART_TIME:
-                    workingHours = 4;
-                    break;
-                default:
-                    workingHours = 0;
-            }
-            dailyWage = (wagePerHour * workingHours);
-            totalWage += dailyWage;
-        }
-        return companyName+" "+totalWage;
-    }
+		while (totalDays <= workingDaysPerMonth && totalHours < maxHours) {
+			totalDays++;
+			int checkWorking = (int) (Math.random() * 3);
+			switch (checkWorking) {
+			case FULL_TIME:
+				workingHours = 8;
+				break;
+			case PART_TIME:
+				workingHours = 4;
+				break;
+			default:
+				workingHours = 0;
+			}
+			dailyWage = (wagePerHour * workingHours);
+			totalWage += dailyWage;
+		}
+		return companyName + " " + totalWage;
+	}
 }
--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,21 +1,26 @@
 package empwagecomputation;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class EmpWageBuilder {
 	public static void main(String[] args) {
-		Object[] employeeWageComputation = new Object[10];
+		ArrayList arrayList = new ArrayList();
 		int index = 0;
 		System.out.println("Welcome to Employee Wage Computation");
 		CompanyEmployeeWage companyEmployeeWage = new CompanyEmployeeWage("Reliance", 20, 20, 50);
-		employeeWageComputation[index++] = companyEmployeeWage.get_EmployeeWageForCompany();
+		arrayList.add(companyEmployeeWage.get_EmployeeWageForCompany());
 
 		companyEmployeeWage = new CompanyEmployeeWage("D-mart", 10, 25, 50);
-		employeeWageComputation[index++] = companyEmployeeWage.get_EmployeeWageForCompany();
+		arrayList.add(companyEmployeeWage.get_EmployeeWageForCompany());
 
 		companyEmployeeWage = new CompanyEmployeeWage("metro", 17, 25, 20);
-		employeeWageComputation[index++] = companyEmployeeWage.get_EmployeeWageForCompany();
+		arrayList.add(companyEmployeeWage.get_EmployeeWageForCompany());
 
-		for (int index1 = 0; index1 < index; index1++) {
-			System.out.println("Total-wage of " + employeeWageComputation[index1]);
+		Iterator iterator = arrayList.iterator();
+		while (iterator.hasNext()) {
+			String string = (String) iterator.next();
+			System.out.println("Employee Wage of Company " + string);
 		}
 	}
 }

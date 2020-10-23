d766907fb7641ad1bf4c90ca0e16cb9aaf816403
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-23 15:39:15 +0530

    Modified to solve switch case statement

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,24 +1,24 @@
 package empwagecomputation;
 
 public class EmpWageComputation {
-	
+
 	public static void main(String[] args) {
 		final int FULLTIME = 1;
 		final int PARTTIME = 0;
-       	int WAGE_PER_HOUR = 20;
-       	int workingHours = 8;
-       	int dailyWage = 0;
+		final int WAGE_PER_HOUR = 20;
+		int workingHours = 8;
+		int dailyWage = 0;
 		int checkWorking = (int) (Math.random() * 3);
 
 		switch (checkWorking) {
-			case FULLTIME:
-				workingHours = 8;
-				break;
-			case PARTTIME:
-				workingHours = 4;
-				break;
-			default:
-				workingHours = 0;
+		case FULLTIME:
+			workingHours = 8;
+			break;
+		case PARTTIME:
+			workingHours = 4;
+			break;
+		default:
+			workingHours = 0;
 		}
 
 		dailyWage = (WAGE_PER_HOUR * workingHours);

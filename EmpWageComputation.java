73f745784750bde4cf0a191cb65dfd0500db3b82
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-23 15:47:46 +0530

    Modified variables to calculate wages for a month

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,14 +1,15 @@
 package empwagecomputation;
 
 public class EmpWageComputation {
-	
+
 	public static void main(String[] args) {
 		final int FULLTIME = 1;
 		final int PARTTIME = 0;
-		int WORKINGDAYS_PER_MONTH = 20;
-       	int WAGE_PER_HOUR = 20;
-       	int workingHours = 8;
-       	int monthlyWage = 0;
+		final int WORKINGDAYS_PER_MONTH = 20;
+		final int WAGE_PER_HOUR = 20;
+		
+		int workingHours = 8;
+		int monthlyWage = 0;
 		int totalDays = 0;
 		int totalHours = 0;
 
@@ -18,17 +19,17 @@ public class EmpWageComputation {
 			int checkWorking = (int) (Math.random() * 3);
 
 			switch (checkWorking) {
-				case FULLTIME:
-					workingHours = 8;
-					break;
-				case PARTTIME:
-					workingHours = 4;
-					break;
-				default:
-					workingHours = 0;
+			case FULLTIME:
+				workingHours = 8;
+				break;
+			case PARTTIME:
+				workingHours = 4;
+				break;
+			default:
+				workingHours = 0;
 			}
 
-			totalHours = totalHours+workingHours;
+			totalHours = totalHours + workingHours;
 		}
 
 		monthlyWage = (WAGE_PER_HOUR * totalHours);
